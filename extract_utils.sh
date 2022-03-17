a01837ee3c08956beb4638850feedfe5af66833c
Chirayu Desai <chirayudesai1@gmail.com>
2022-03-17 06:27:33 +0530

    Use simg2img from prebuilts/extract-tools
    Change-Id: I15772394a24c2ff1fdd6eece86548a72a5d4d748

--- extract_utils.sh
+++ extract_utils.sh
@@ -96,6 +96,7 @@ function setup_vendor() {
 
     export BINARIES_LOCATION="$ANDROID_ROOT"/prebuilts/extract-tools/${HOST}-x86/bin
 
+    export SIMG2IMG="$BINARIES_LOCATION"/simg2img
     export SIGSCAN="$BINARIES_LOCATION"/SigScan
 
     for version in 0_8 0_9; do
@@ -1428,7 +1429,7 @@ function extract() {
                 if [[ $(file -b "$IMAGE") == Linux* ]]; then
                     extract_img_data "$IMAGE" "$DUMPDIR"/"$PARTITION"
                 elif [[ $(file -b "$IMAGE") == Android* ]]; then
-                    simg2img "$IMAGE" "$DUMPDIR"/"$PARTITION".raw
+                    "$SIMG2IMG" "$IMAGE" "$DUMPDIR"/"$PARTITION".raw
                     extract_img_data "$DUMPDIR"/"$PARTITION".raw "$DUMPDIR"/"$PARTITION"/
                 else
                     echo "Unsupported "$IMAGE""
@@ -1672,7 +1673,7 @@ function generate_prop_list_from_image() {
     if [[ $(file -b "$image_file") == Linux* ]]; then
         extract_img_data "$image_file" "$image_dir"
     elif [[ $(file -b "$image_file") == Android* ]]; then
-        simg2img "$image_file" "$image_dir"/"$(basename "$image_file").raw"
+        "$SIMG2IMG" "$image_file" "$image_dir"/"$(basename "$image_file").raw"
         extract_img_data "$image_dir"/"$(basename "$image_file").raw" "$image_dir"
         rm "$image_dir"/"$(basename "$image_file").raw"
     else
